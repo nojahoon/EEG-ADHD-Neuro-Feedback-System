77b72c521dc6649d63f5a3c2badd61ad399f9e2f
nojahoon <59651691+nojahoon@users.noreply.github.com>
2021-02-05 00:46:12 +0900

    const

---
+++ Game/const.py
@@ -0,0 +1,14 @@
+#### STAGE ####
+GAME_LOBBY=1    #Lobby 화면
+GAME_PLAY=2     #게임 진행단계
+GAME_PAUSE=3    #게임 멈춤단계
+GAME_END=4      #게임 종료단계
+
+#### Time Control ####
+GAME_LOOP=90    #게임컨트롤 시간 sec
+
+#### Shape ####
+GAME_SHAPE=4    #도형의 개수
+
+
+
