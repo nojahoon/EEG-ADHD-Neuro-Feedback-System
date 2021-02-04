cd0357eeeb632ce47da7b00005b65e6c439f7435
nojahoon <59651691+nojahoon@users.noreply.github.com>
2021-02-05 01:31:05 +0900

    font update

--- Game/hello.py
+++ Game/hello.py
@@ -3,7 +3,9 @@ import sys
 
 # 지정 위치 텍스트 출력
 def Show_text(x, y, text):
-    sf = pygame.font.SysFont("Monospace", 50, bold = True)
+    #sf = pygame.font.SysFont("Monospace", 50, bold = True)
+    sf = pygame.font.Font('font/CookieRun Bold.ttf', 30, bold=True)
+    #pygame.font.Font('image/CookieRun Bold.ttf', heightSize // size)
     title_str = text
     title = sf.render(title_str, True, white)
     title_size = sf.size(title_str)
@@ -28,7 +30,7 @@ pos_y = 200
 clock = pygame.time.Clock()
 while run:
     screen.fill(black)
-    Show_text(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, "Start!")
+    Show_text(SCREEN_WIDTH//2, SCREEN_HEIGHT//4, "색상 게임에 오신것을 환영합니다.")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # 애플리케이션을 종료하고자 하면
             run = False
